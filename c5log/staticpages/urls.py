07d440117e414070d24702f642e358a184be829d
Blake Bengtson <bbeng89@gmail.com>
2013-11-25 19:51:24 -0600

    Adding a page for bot commands. Added message about taking down public access

--- c5log/staticpages/urls.py
+++ c5log/staticpages/urls.py
@@ -4,4 +4,5 @@ from staticpages import views
 
 urlpatterns = patterns('',
 	url(r'^about/$', views.AboutView.as_view(), name="about"),
+	url(r'^bot/$', views.BotView.as_view(), name="bot")
 )
--- c5log/staticpages/views.py
+++ c5log/staticpages/views.py
@@ -3,3 +3,8 @@ from django.views.generic.base import TemplateView
 class AboutView(TemplateView):
 
 	template_name = "about.html"
+
+
+class BotView(TemplateView):
+
+	template_name = "bot.html"
